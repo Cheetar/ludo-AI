18da5887192438490d99bb5dafb3b0c6cd79421b
td236 <dadela.tomasz@gmail.com>
2020-07-14 17:18:47 +0200

    website: crispy forms: submit & login & logout

--- site/ludo/ludo/settings.py
+++ site/ludo/ludo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'main.apps.MainConfig',
     'register.apps.RegisterConfig',
 ]
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
