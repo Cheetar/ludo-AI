b3f4d897e0be4a15474b1d7ee314cd459bc070fe
td236 <dadela.tomasz@gmail.com>
2020-07-15 19:55:42 +0200

    website: contest.models: new class Participant & other class changes

--- site/ludo/contest/models.py
+++ site/ludo/contest/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 from main.models import Code
 
-'''
+
 class Participant(Code):
-    father = models.IntegerField()??
+    father_id = models.IntegerField()
     no_played_games = models.IntegerField(default=0)
-    local_rating = models.IntegerField(default=1200)
-    score = models.IntegerField(default=0)
-'''
+    local_rating = models.FloatField(default=1200.)
+    score = models.FloatField(default=0.) #
+
+    '''
+    def no_played_games_add_1(self): self.no_played_games+=1
+    def change_local_rating(self, change): self.local_rating+=change
+    def change_score(self, change): self.score+=change
+    '''
+
 
 class Contest(models.Model):
     title = models.CharField(max_length=200)
-    participants = models.ManyToManyField(Code)
+    participants = models.ManyToManyField(Participant)
+    ranking_game = models.BooleanField(default=True)
 
-    #def add_participant(id: int):
-    #    participants.append()
+    def __str__(self):
+        return self.title
+
+    #def add_participant(self, p: Participant): self.participants.add(p)
--- site/ludo/main/models.py
+++ site/ludo/main/models.py
@@ -8,6 +8,18 @@ class Code(models.Model):
     title = models.CharField(max_length=200)
     code = models.CharField(max_length=20000)
     rating = models.IntegerField(default=1200)
+    public = models.BooleanField(default=True)
+
+    #type
+    #no round
+
+    class Meta:
+        order_with_respect_to = 'rating'
 
     def __str__(self):
         return self.title
+
+    '''
+    def set_title(self, t): self.title = t
+    def set_code(self, c): self.code = c
+    '''
