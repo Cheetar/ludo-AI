61c2701fa72d51d583939faf48ce9312ed91091a
td236 <dadela.tomasz@gmail.com>
2021-02-10 23:39:46 +0100

    setup.sh & run.sh update

--- site/setup.sh
+++ site/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo 'creating virtual environment'
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv venv || { echo 'creating virtual environment failed' ; exit 1; }
+source venv/bin/activate || { echo 'activating virtual environment failed' ; exit 1; }
 
 echo 'python libraries installation'
 pip3 install -r requirements.txt
